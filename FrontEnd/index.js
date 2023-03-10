610e0b2775b2dd014afb544d68d8bd83b6ae49bd
CR <celineregnier2003@yahoo.fr>
2023-03-10 15:22:11 +0100

    ve 100323

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,1 +1,10 @@
-alert("work")
+const myImage = document.querySelector('img');
+
+fetch('flowers.jpg')
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  const objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
