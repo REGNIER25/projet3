9692cf34618898229fe064a7ace31d1f43b8c6ae
CR <celineregnier2003@yahoo.fr>
2023-03-15 13:44:57 +0100

    me 150323

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -8,24 +8,22 @@ fetch("http://localhost:5678/api/works")
   })
 
   .then(function(works) {
-    console.log(works);
     for(let work of works) {
-      console.log (work.title);
       let figure = document.createElement("figure");
-      gallery.appendChild(figure);
+     
 
       let img = document.createElement("img");
-      figure.appendChild(img);
       img.setAttribute("src", work.imageUrl);
       img.setAttribute("alt", work.title);
+      figure.appendChild(img);
 
       let figcaption = document.createElement("figcaption");
+      figcaption.innerHTML = work.title;
       figure.appendChild(figcaption); 
 
-      //elem.appendChild(document.createTextNode("Texte"));// 
-     // set.figcaption.add //
-      // figcaption.setAttribute (figcaption, work.title);*
-      /*figcaption.innerHTML = `${work.title.first} ${work.title.last}`*/ 
+      gallery.appendChild(figure);
+
+       
     }
   })
    
