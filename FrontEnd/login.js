94d84d7b6e5cfe58ebca99fb4899ad5015442562
CR <celineregnier2003@yahoo.fr>
2023-03-25 15:56:09 +0100

    sa 250323

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,24 @@
 
+//PREMIER APPEL
+//url et options (méthode, en-tête de requête, corps de la demande avec données à envoyer en tant que string)
+
+let response = await fetch("http://localhost:5678/api/users/login", {
+  method: "POST",
+  headers: {"accept":"application/json","Content-type":"text/plain"},
+  body: JSON.stringify(user)});
+
+//DEUXIEME APPEL
+//réponse du back-end (promise) en lisant le body en JSON (traitement)
+//Pour obtenir le corps de réponse : analyse la réponse en tant qu’objet JSON
+
+  let result = await response.json(); 
+
+
+//si erreur
+.catch(function (error) {
+   (error);
+ });
+
 
 // Envoi du formulaire
 formulaire.addEventListener("submit", function(event) {
@@ -6,37 +26,55 @@ formulaire.addEventListener("submit", function(event) {
 // Récupération du formulaire 
 const formulaire = document.querySelector("form");
 
-// Récupèration des champs email et password
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+// Récupèration des champs email et password (données du formulaire)
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
-    // Validation des données
-    let erreurs = validerPersonne(email.value, password.value);
+// l'evenement permet de détecter sur quel composant le clic est passé   
+input.onclick.addEventListener= function(submit) {
+   ('click', event => {alert("");});};
+
+ // Envoi du formulaire
+ formulaire.submit();
+
+
+//création variables login (email et password)
+let email= " ";
+let password = " ";
+
+ // Validation des données
+
+    function validerPersonne(email, password) {
+//création tableau login
+
+
+      let tabloLogin = [(email),(password)];}
+
+      //Créer variable token  
+var token = sessionStorage.getItem("token");
+
+//"userId": 1,
   
-    // Si il y a des erreurs
-    if (erreurs.length > 0) {
-       // Ajoute les erreurs à la fin de ul.message
-       ajouterFinListe(eleMessage, erreurs);
+    // Si il y a des erreurs (conditions)
+   // email != "" || password !="")
+
+    .then(valid => {if (!valid) {
+       //Si combinaison est fausse, message d'erreur
+       {alert("Le nom de l'utilisateur et/ou le mot de passe sont incorrects !");
     } else {
-       // Envoi du formulaire
-       eleFormulaire.submit();
+
+//vérifier si les deux sont bons puis renvoyer le token
+result.status(200).json({
+   userId: "userId",
+   token: "token"});})
+
+      //Enregistrer le token d'authentification dans le sessionStorage (navigateur)
+//sessionStorage.setItem('clé', 'valeur');
+sessionStorage.setItem("token", token);
+
+//Redirection vers la page d’accueil quand la connexion est confirmée via le token
+function redirection(if token!=null){document.location.href="index.html"; }
+      
     }
     
   });
-
-
-
-
-
-
-
-
-//Récupération du token dans la session
-var token = sessionStorage.getItem("token");
-
-
-//Redirection vers la page d’accueil quand la connexion est confirmée.
-function redirection(){document.location.href="index.html"; }
-
-//Si combinaison est fausse, message d'erreur
-alert("Le nom de l'utilisateur et/ou le mot de passe sont incorrects !");
