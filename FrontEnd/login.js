bdabcd5a7a0b1328847b7daeb939694d23c8744c
CR <celineregnier2003@yahoo.fr>
2023-03-31 16:05:12 +0200

    ve 310323

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -12,44 +12,38 @@
       let inputPassword = document.getElementById("password").value;
 
     //3) Formater les valeurs pour les envoyer vers le serveur
-let tabloSubmit = {inputEmail, inputPassword};
+let tabloSubmit = {email:inputEmail, password:inputPassword};
 
-// //4) Envoyer les données formatées du formulaire au serveur
-// let response = await fetch("http://localhost:5678/api/users/login", {
-//   method: "POST",
-//   headers: { "accept": "application/json" , "Content-type": "application/json" },
-//   body: JSON.stringify(tabloSubmit)
-// });
+//4) Envoyer les données formatées du formulaire au serveur
+let response = await fetch("http://localhost:5678/api/users/login", {
+method: "POST",
+headers: { "accept": "application/json" , "Content-type": "application/json" },
+body: JSON.stringify(tabloSubmit)
+});
+    
 
 // //5) Traitement de la réponse et renvoi vers la page d'accueil si bonne combinaison
-// let result = await response.json();
-    
-      // Afficher les valeurs
-      console.log(tabloSubmit);
-    }
+let result = await response.json();
+   
+
+function validationUser() {
+
+      //Si mauvaise combinaison
+      //bloquer le script
+if (result.status == 401) {
+  alert("Le nom de l'utilisateur et/ou le mot de passe sont incorrects !"); 
+}
 
 
 
-
-//Si mauvaise combinaison
-//if (result.status !== 200) { 
   
-  //alert("Le nom de l'utilisateur et/ou le mot de passe sont incorrects !"); 
-
-//}
-
-//Si bonne combinaison
-//else {
-
-  //result.status(200).json(userId, token)
-
-//};
-
-  //Création Storage (local ou session ?) avec stockage du token
-  //function creationSession()
-
-  //let token = sessionStorage.getItem("token");
+// Si bonne combinaison
+//Redirection vers la Page d'accueil quand la connexion est confirmée via le token
+//Création localStorage avec stockage du token
+else (result.status==200) 
 
 
-  //Redirection vers la Page d'accueil quand la connexion est confirmée via le token
-  //function redirection(token!== null) { document.location.href = "index.html"; };
+{localStorage.getItem(userId,token), document.location.href = "index.html"};
+
+  
+}}
