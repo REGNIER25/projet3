2441b6187ca1b82a3d4edd8a12a9c160e12d383b
CR <celineregnier2003@yahoo.fr>
2023-04-01 14:07:59 +0200

    page de connexion

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,49 +1,41 @@
+const boutonValider = document.getElementById("submit");
 
+//1) Détecter le clic sur le bouton de validation
+boutonValider.onclick = async function (event) {
+  event.preventDefault();
 
+  //2) Récupérer l'ensemble des valeurs du formulaire
+  let inputEmail = document.getElementById("email").value;
+  let inputPassword = document.getElementById("password").value;
 
-  const boutonValider = document.getElementById("submit");
+  //3) Formater les valeurs pour les envoyer vers le serveur
+  let tabloSubmit = { email: inputEmail, password: inputPassword };
 
-  //1) Détecter le clic sur le bouton de validation
-    boutonValider.onclick = async function(event){
-    event.preventDefault();
+  //4) Envoyer les données formatées du formulaire au serveur
+  let response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "accept": "application/json", "Content-type": "application/json" },
+    body: JSON.stringify(tabloSubmit)
+  });
 
-//2) Récupérer l'ensemble des valeurs du formulaire
-      let inputEmail = document.getElementById("email").value;
-      let inputPassword = document.getElementById("password").value;
+  // //5) Traitement de la réponse
+  // Si bonne combinaison, création localStorage 
+  //avec stockage du token et redirection vers l'accueil
+  if (response.ok === true) {
 
-    //3) Formater les valeurs pour les envoyer vers le serveur
-let tabloSubmit = {email:inputEmail, password:inputPassword};
+    let result = await response.json()
+    localStorage.setItem("token", result.token)
+    document.location.href = "index.html"
 
-//4) Envoyer les données formatées du formulaire au serveur
-let response = await fetch("http://localhost:5678/api/users/login", {
-method: "POST",
-headers: { "accept": "application/json" , "Content-type": "application/json" },
-body: JSON.stringify(tabloSubmit)
-});
-    
+  }
 
-// //5) Traitement de la réponse et renvoi vers la page d'accueil si bonne combinaison
-let result = await response.json();
-   
+  //Si mauvaise combinaison
+  else {
 
-function validationUser() {
+    alert("Le nom de l'utilisateur et/ou le mot de passe sont incorrects !");
 
-      //Si mauvaise combinaison
-      //bloquer le script
-if (result.status == 401) {
-  alert("Le nom de l'utilisateur et/ou le mot de passe sont incorrects !"); 
+  }
+
 }
 
 
-
-  
-// Si bonne combinaison
-//Redirection vers la Page d'accueil quand la connexion est confirmée via le token
-//Création localStorage avec stockage du token
-else (result.status==200) 
-
-
-{localStorage.getItem(userId,token), document.location.href = "index.html"};
-
-  
-}}
